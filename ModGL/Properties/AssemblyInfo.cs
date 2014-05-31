7e9528afd58a2099dd37e8622a999904c53acdea
Henning Moe <henning.moe@hotmail.com>
2014-05-31 15:35:15 +0200

    Update assemblyinfo.

--- ModGL/Properties/AssemblyInfo.cs
+++ ModGL/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("General Public")]
 [assembly: AssemblyProduct("ModGL")]
-[assembly: AssemblyCopyright("Copyright © Henning Moe 2013")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +32,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-prerelease")]
 
